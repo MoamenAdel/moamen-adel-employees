7c2e89bd8e29f9f9b647e00be824b62845513ecd
MoamenAdel <moamenadel94@gmai.com>
2022-01-01 16:48:40 +0200

    Adding @CrossOrigin annotation to enable uploading from angular ui project

--- src/main/java/com/example/evolvice/rest/RestApiController.java
+++ src/main/java/com/example/evolvice/rest/RestApiController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class RestApiController {
 	private IUtilityService utilityService;
 
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping
 	public ResponseEntity<Void> addFile(@RequestBody() MultipartFile file) throws IOException {
 		utilityService.processFile(file);
